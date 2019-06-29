5662c895b0b7a6c1fef414e8092d9f5b1b1aece1
bluelogix <bluelogix010@gmail.com>
2019-06-29 12:59:43 -0400

    adding first files

---
+++ api /server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// const authRouter = require('../auth/auth-routes.js')
+// const articleRouter = require('../articles/article-routes.js')
+// const { authenticate } = require('../auth/authenticate');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// test endpoint
+server.get('/', (req, res) => {
+  res.status(200).json({message: 'server up!'})
+})
+
+// server.use('/api', authRouter);
+// server.use('/api', authenticate, articleRouter);
+
+module.exports = server;
